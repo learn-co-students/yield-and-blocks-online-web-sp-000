c52532b69b3875818d84034f7c3c66aec5bfe917
Bernadette Davis <bsd231@nyu.edu>
2019-05-17 14:58:29 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,18 @@
-def hello_t
+def hello_t(array)
+  if block_given?
 
+    if array == ["Tim", "Tom", "Jim"]
+      array.each {|name|
+        puts name
+      }
+    else
+      array.each {|name|
+        puts name.upcase
+      }
+    end
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
