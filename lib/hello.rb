5091db544f2942c3f4b7516fba2bc1748d4efdf3
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-27 18:51:42 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,19 @@
 def hello_t(array)
+  if block_given?
+    #used with the "else" statement below
   i = 0
     while i < array.length
       #yield each member of array  to the block that is passed into hello_t
       yield array[i]
-      i += 1
-    end
-end
+      i = i + 1
 
+end
+array
+#return original array instead of nil
+  else
+    puts "Hey! No block was given!"
+  end
+end
 # call your method here!
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
