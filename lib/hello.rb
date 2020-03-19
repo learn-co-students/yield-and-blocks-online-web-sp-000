3c81d81e0c061033572ec28c705a1705c5059142
santiago molinari <santomolinari@gmail.com>
2020-03-19 16:08:13 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
-
+def hello_t(array)
+    counter = 0
+    if block_given?
+        while counter < array.length
+            yield(array[counter])
+            counter += 1
+        end
+        array
+    else
+        puts "Hey! No block was given!"
+    end
 end
+
+
 
 # call your method here!
 
