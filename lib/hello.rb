ad1ccb4636da0c6f0b14bca1a16ccd64e960b1c4
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-09-22 02:32:41 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,9 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    array.each { |element|
+      yield(element)
+    }
+  else
+    puts "Hey! No block was given!"
+  end
 end
-
-# call your method here!
-
